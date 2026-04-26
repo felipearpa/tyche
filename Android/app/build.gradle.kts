5dcd055d14f0456817ab51292bcecb1ab995d584
Felipe Arcila Parra <felipearcila@gmail.com>
2026-04-26 11:55:44 -0500

    Increment version code and name to 2 and 1.1

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
     defaultConfig {
         applicationId = "com.felipearpa.fortuna"
         minSdk = projectMinSdk.toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
