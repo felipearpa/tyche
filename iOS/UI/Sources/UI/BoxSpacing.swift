a7fd94b3feac9f664be0271ab1b87590e6c2ecfc
Felipe Arcila Parra <felipearcila@gmail.com>
2026-05-17 17:46:27 -0500

    Add `edit_username_subtitle` to the username editor on Android and iOS, remove username required error messaging, and refine UI styling for font sizes and component spacing.

--- iOS/UI/Sources/UI/BoxSpacing.swift
+++ iOS/UI/Sources/UI/BoxSpacing.swift
@@ -4,4 +4,9 @@ public struct BoxSpacing {
     public let small: CGFloat
     public let medium: CGFloat
     public let large: CGFloat
+    public let extraLarge: CGFloat
+    public let xxLarge: CGFloat
+    public let xxxLarge: CGFloat
+    public let huge: CGFloat
+    public let massive: CGFloat
 }
